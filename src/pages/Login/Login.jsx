6ee6d0e93188f12502a9585eb355ea002ade0466
nmkhans <nurmoin05@gmail.com>
2023-01-29 22:19:49 +0600

    login form added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const Login = () => {
     return (
         <div className="Login">
             <div className="container">
-                
+                <div className="login__title mt-5 mx-auto text-center">
+                    <h3 className="font-semibold d-inline-block border-2 border-bottom border-dark pb-2">Login</h3>
+                </div>
+                <div className="login__content w-75 mx-auto mt-5 border border-gray p-4 rounded">
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
             </div>
         </div>
     );
